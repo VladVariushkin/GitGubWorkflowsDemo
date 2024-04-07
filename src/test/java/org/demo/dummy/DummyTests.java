3c929eaebcf07637b6e1e69ad397c90597373a1e
VladVariushkin <61284170+VladVariushkin@users.noreply.github.com>
2024-04-07 23:27:17 +0300

    Test/pr branch (#3)
    pr-workflow fix

--- src/test/java/org/demo/dummy/DummyTests.java
+++ src/test/java/org/demo/dummy/DummyTests.java
@@ -1,10 +1,12 @@
 package org.demo.dummy;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class DummyTests {
 
+    @Disabled
     @Test
     void testFail() {
         Assertions.fail();
